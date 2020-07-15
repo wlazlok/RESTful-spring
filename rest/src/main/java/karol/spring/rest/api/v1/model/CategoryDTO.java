ebd02a12b8eb3dd140c24201b6326c2d1fc28b17
unknown <kwlazo9@gmail.com>
2020-07-15 09:50:38 +0200

    first commit

---
+++ rest/src/main/java/karol/spring/rest/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package karol.spring.rest.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ rest/src/main/java/karol/spring/rest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package karol.spring.rest.repositories;
+
+import karol.spring.rest.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
