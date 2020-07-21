3ff228d37bec6771e795017ca27e0f81fa33e504
unknown <kwlazo9@gmail.com>
2020-07-21 08:44:31 +0200

    first commit fruit-shop-vendors0api

---
+++ fruit-shop-vendors-api/src/main/java/karolspring/fruitshopvendorsapi/FruitShopVendorsApiApplication.java
@@ -0,0 +1,13 @@
+package karolspring.fruitshopvendorsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitShopVendorsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitShopVendorsApiApplication.class, args);
+	}
+
+}
---
+++ fruit-shop-vendors-api/src/test/java/karolspring/fruitshopvendorsapi/FruitShopVendorsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package karolspring.fruitshopvendorsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitShopVendorsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
