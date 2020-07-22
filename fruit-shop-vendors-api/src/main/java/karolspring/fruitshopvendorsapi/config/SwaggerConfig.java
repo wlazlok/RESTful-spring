9427424056f3cbd5ff54f11034a5b25bc58846ef
unknown <kwlazo9@gmail.com>
2020-07-22 08:45:40 +0200

    Swagger metadata config

--- fruit-shop-vendors-api/src/main/java/karolspring/fruitshopvendorsapi/config/SwaggerConfig.java
+++ fruit-shop-vendors-api/src/main/java/karolspring/fruitshopvendorsapi/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 
 @EnableSwagger2
@@ -19,6 +23,23 @@ public class SwaggerConfig {
                 .select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Karol Wlazlo", "None", "kwlazo9@gmail.com");
+
+        return new ApiInfo(
+                "Karol Spring",
+                "Spring 5",
+                "1.0",
+                "BLAH",
+                contact,
+                "Apache License Version 2.0",
+                "url..",
+                new ArrayList<>()
+        );
     }
 }
