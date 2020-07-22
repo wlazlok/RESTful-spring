ab0bbb2638b121e935f07675ca11a91287501a81
unknown <kwlazo9@gmail.com>
2020-07-22 09:11:19 +0200

    upload new project

---
+++ webflux-rest/src/main/java/karol/spring/webfluxrest/WebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package karol.spring.webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxRestApplication.class, args);
+    }
+
+}
---
+++ webflux-rest/src/test/java/karol/spring/webfluxrest/WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package karol.spring.webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
